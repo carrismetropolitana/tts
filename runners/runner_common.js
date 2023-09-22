0a1f06d81011729f089d0971993eb405b7921e04
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-09-23 00:51:25 +0100

    Added common sayings

--- runners/runner_common.js
+++ runners/runner_common.js
@@ -7,51 +7,52 @@ const GoogleCloudTTSAPI = require('../services/GoogleCloudTTSAPI');
 module.exports = async () => {
   console.log();
   console.log('* * * * * * * * * * * * * * * * * * * * * * * * * *');
-  console.log('* TTS STOPS');
+  console.log('* TTS COMMON');
   const start = new Date();
   console.log(`* Run started on ${start.toISOString()}`);
 
   // Setup tracker
-  const trackerData = Tracker.get('stops');
+  const trackerData = Tracker.get('common');
   const trackerDataUpdated = [];
 
-  // Get all stops
-  console.log('* Fetching all stops from API...');
-  const allStopsResponse = await fetch('https://api.carrismetropolitana.pt/stops');
-  const allStopsData = await allStopsResponse.json();
+  // Define common sayings
+  const allCommonData = [
+    { id: 'next_stop', text: 'Seguinte' },
+    { id: 'last_stop', text: 'Fim de Percurso. Obrigado por viajar com a Carris Metropolitana!' },
+  ];
 
   // Log progress
-  console.log(`* Preparing ${allStopsData.length} stops...`);
+  console.log(`* Preparing ${allCommonData.length} common sayings...`);
   console.log();
 
-  // Iterate on each stop
-  for (const [stopIndex, stopData] of allStopsData.entries()) {
+  // Iterate on each common saying
+  for (const [commonIndex, commonData] of allCommonData.entries()) {
     //
 
     // Check if tracker already has this entry,
     // and if it differs from the given TTS.
-    const trackerEntry = trackerData.find((item) => item.id === stopData.id);
-    const ttsHasChanged = stopData.tts_name !== trackerEntry?.tts;
+    const trackerEntry = trackerData.find((item) => item.id === commonData.id);
+    const ttsHasChanged = commonData.text !== trackerEntry?.tts;
 
     if (ttsHasChanged) {
-      await GoogleCloudTTSAPI({ string: stopData.tts_name, filename: stopData.id, dirname: `${settings.OUTPUTS_DIRNAME}/stops`, replaceIfExists: true });
-      console.log(`* [${stopIndex}/${allStopsData.length}] Generated | Stop ${stopData.id} | ${stopData.tts_name}`);
+      await GoogleCloudTTSAPI({ string: commonData.text, filename: commonData.id, dirname: `${settings.OUTPUTS_DIRNAME}/common`, replaceIfExists: true });
+      console.log(`* [${commonIndex}/${allCommonData.length}] Generated | Stop ${commonData.id} | ${commonData.text}`);
     }
 
-    trackerDataUpdated.push({ id: stopData.id, tts: stopData.tts_name });
+    trackerDataUpdated.push({ id: commonData.id, tts: commonData.text });
 
     //
   }
 
   // Save updated tracker
-  Tracker.set('stops', trackerDataUpdated);
+  Tracker.set('common', trackerDataUpdated);
 
   // Clean directory
-  Tracker.clean('stops');
+  Tracker.clean('common');
 
   //
   console.log();
-  console.log(`* Processed ${trackerDataUpdated.length} stops.`);
+  console.log(`* Processed ${trackerDataUpdated.length} common.`);
   const syncDuration = new Date() - start;
   console.log(`* Run took ${syncDuration / 1000} seconds.`);
   console.log('* * * * * * * * * * * * * * * * * * * * * * * * * *');
